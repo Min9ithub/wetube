73a3ebd4e92e789754fe092e7678900d7c5e8c37
Nam Min Ki <nam960216@gmail.com>
2022-04-16 14:35:21 +0900

    js

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
   see,
-  edit,
+  getEdit,
+  postEdit,
   upload,
   deleteVideo,
   watch,
@@ -10,8 +11,7 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
