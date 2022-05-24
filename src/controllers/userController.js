78c0da8aeafb81223f25bd794f65ef6de72f37e8
Nam Min Ki <nam960216@gmail.com>
2022-05-24 13:13:56 +0900

    8.0

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -42,7 +42,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -111,6 +111,7 @@ export const finishGithubLogin = async (req, res) => {
       (email) => email.primary === true && email.verified === true
     );
     if (!emailObj) {
+      //set notification
       return res.redirect("/login");
     }
 
@@ -121,6 +122,7 @@ export const finishGithubLogin = async (req, res) => {
       return res.redirect("/");
     } else {
       const user = await User.create({
+        avatarUrl: userData.avatar_url,
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
@@ -129,7 +131,7 @@ export const finishGithubLogin = async (req, res) => {
         location: userData.location,
       });
       req.session.loggedIn = true;
-      req.session.user = existingUser;
+      req.session.user = user;
       return res.redirect("/");
     }
   } else {
@@ -137,7 +139,17 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Log Out");
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", {
+    pageTitle: "Edit Profile",
+    user: req.session.user,
+  });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+};
 export const see = (req, res) => res.send("See User");
