155985b36d23cb26d187a45f7f9ef59b2ac99643
Minki <nam960216@gmail.com>
2022-06-29 22:38:18 +0900

    db

--- src/db.js
+++ src/db.js
@@ -7,5 +7,7 @@ const db = mongoose.connection;
 const handleOpen = () => console.log("✔ Connected to DB ");
 const handleError = (error) => console.log("❌ DB Error", error);
 
-db.on("error", handleError);
 db.once("open", handleOpen);
+db.on("error", handleError);
+
+// once는 한번만 나타남, on은 여러번 나타낼 수 있음
