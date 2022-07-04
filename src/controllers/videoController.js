c7ef4eb7a5aebcf446089a2378cbf69ee3a947a3
Minki <nam960216@gmail.com>
2022-07-04 17:21:34 +0900

    videoController

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,7 +9,7 @@ Video.find({}, (error, videos) => {
 export const home = async (req, res) => {
   // Video.find({}) -> {} 안이 비어있으면 모든 형식을 찾는다는 것을 뜻한다.
   const videos = await Video.find({}).sort({ createdAt: "desc" });
-  return res.status(404).render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = async (req, res) => {
@@ -26,7 +26,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.status(404).render("404", { pageTitle: "Video not found" });
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
@@ -34,6 +34,7 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
+  // Model.exists(_id : id) --> id의 존쟈 여부만을 확인한다.-> 아랫줄에 !video가 true인지 false인지 나타내기 위해서
   const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found" });
@@ -43,7 +44,6 @@ export const postEdit = async (req, res) => {
     description,
     hashtags: Video.formatHashtags(hashtags),
   });
-
   return res.redirect(`/videos/${id}`);
 };
 
@@ -70,6 +70,7 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
+  // Video.findByIdAndRemove()도 있지만 특별한 이유가 없는 이상 findByIdAndDelete를 사용()
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
@@ -80,6 +81,7 @@ export const search = async (req, res) => {
   if (keyword) {
     videos = await Video.find({
       title: {
+        // i는 ignore의 약자로 대소문자 무시를 의미한다
         $regex: new RegExp(keyword, "i"),
       },
     });
