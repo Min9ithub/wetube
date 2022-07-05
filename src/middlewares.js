5cc626602f15e5a13e090af16cbac87a53e464ed
Minki <nam960216@gmail.com>
2022-07-05 23:11:11 +0900

    middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
+  // locals object는 전역변수이기 때문에 pug에서 사용 가능하다.
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
