8ad743f73c0b11cc2a19f1094c0f11791ea7b28d
Minki <nam960216@gmail.com>
2022-07-20 23:27:04 +0900

    middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,5 @@
+import multer from "multer";
+
 export const localsMiddleware = (req, res, next) => {
   // locals object는 전역변수이기 때문에 pug에서 사용 가능하다.
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -13,6 +15,7 @@ export const protectorMiddleware = (req, res, next) => {
     return res.redirect("/login");
   }
 };
+
 export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
@@ -20,3 +23,16 @@ export const publicOnlyMiddleware = (req, res, next) => {
     return res.redirect("/");
   }
 };
+
+export const avatarUpload = multer({
+  dest: "uploads/avatars/",
+  limits: {
+    fileSize: 3000000,
+  },
+});
+export const videoUpload = multer({
+  dest: "uploads/videos/",
+  limits: {
+    fileSize: 10000000,
+  },
+});
