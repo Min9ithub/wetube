9b6aea8c50d110c3d8ec39f89b7b63bd0ed5a7be
Minki <nam960216@gmail.com>
2022-08-06 15:05:23 +0900

    routers

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,14 +7,32 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
+import { protectorMiddleware, uploadFiles, videoUpload } from "../middlewares";
 
 const videoRouter = express.Router();
 
-// ([0-9a-f]{24}) --> 0부터 9 그리고 a부터 f까지의(16진수) 24자 string. 
+// ([0-9a-f]{24}) --> 0부터 9 그리고 a부터 f까지의(16진수) 24자 string.
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(
+    videoUpload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "thumb", maxCount: 1 },
+    ]),
+    postUpload
+  );
 
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
