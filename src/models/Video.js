f83d9824f0eb4cf50ac432d1ab8702df1c3fd719
Minki <nam960216@gmail.com>
2022-08-06 15:03:44 +0900

    models

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,13 +4,16 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxlength: 80 },
-  description: { type: String, required: true, trim: true, minlength: 20 },
+  fileUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minlength: 2 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
-    rating: { type: Number, default: 0, required: true },
   },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 // formatHashtags라는 메소드 만들기
