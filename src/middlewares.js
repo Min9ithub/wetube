327ef29b59f020f0df389198a4b14c898232801e
Minki <nam960216@gmail.com>
2022-08-07 12:22:52 +0900

    middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,19 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-minki",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   // locals object는 전역변수이기 때문에 pug에서 사용 가능하다.
@@ -31,10 +46,13 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
+
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
