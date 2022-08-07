371bd26b86d9fc5a265559284ec766adb1bad08a
Minki <nam960216@gmail.com>
2022-08-07 16:35:45 +0900

    middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,6 +9,8 @@ const s3 = new aws.S3({
   },
 });
 
+const isHeroku = process.env.NODE_ENV === "production";
+
 const s3ImageUploader = multerS3({
   s3: s3,
   bucket: "wetube-minki/images",
@@ -26,6 +28,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -52,7 +55,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: s3ImageUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -60,5 +63,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: s3VideoUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
