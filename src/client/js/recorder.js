e7e55def06b9983a05f85b7a5a94870a54592792
Nam Min Ki <nam960216@gmail.com>
2022-08-16 21:41:27 +0900

    20220815

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -26,7 +26,7 @@ const handleDownload = async () => {
   actionBtn.disabled = true;
 
   const ffmpeg = createFFmpeg({
-    corePath: "https://unpkg.com/@ffmpeg/core@0.8.5/dist/ffmpeg-core.js",
+    // corePath: "https://unpkg.com/@ffmpeg/core@0.8.5/dist/ffmpeg-core.js",
     log: true,
   });
   await ffmpeg.load();
